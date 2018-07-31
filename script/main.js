2c2282e7abc174a45a9357d91e6c7b05c9553def
ItzhakS <ieshachnovitz@gmail.com>
2018-07-31 12:48:33 +0300

    Fix post data songs array

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 $(function() {
 
   $('.new-Playlist-Icon').click(()=>{
-    $('.new-PlaylistWrapper').dialog({modal: true, width: 500});
+    $('.new-PlaylistWrapper').dialog({modal: true, width: 500, title:'Add New Playlist'});
     $('.newSongs').hide();
     $('.new-PlaylistContainer').show();
 
@@ -10,6 +10,7 @@ $(function() {
   $('.next-btn').click(()=>{
     event.preventDefault();
     $('.new-PlaylistContainer').hide("fade", 500, ()=>{
+      $('.new-PlaylistWrapper').dialog({width: 600, title:'Add Playlist Songs'});
       $('.newSongs').show("fade", 500);
     });
   });
@@ -20,8 +21,8 @@ $(function() {
     data.name = $('input[name="playlistName"]').val();
     data.image = $('input[name="image"]').val();
     data.songs = [];
-    $('.addSongs').each(()=>{
-      let songObj = {"url":`${$('input[name="url"]').val()}`, "name":`${$('input[name="songName"]').val()}`};
+    $('.addSongs').each(function() {
+      let songObj = {"url":`${$(this).find('input[name="url"]').val()}`, "name":`${$(this).find('input[name="songName"]').val()}`};
       data.songs.push(songObj);
     })
     // let getData = {'type': 'playlist'};
@@ -30,18 +31,18 @@ $(function() {
     post.done(()=>{
       alert('Posted!!');
     });
-    $('.new-PlaylistWrapper').dialog();
+    $('.new-PlaylistWrapper').dialog('close');
   });
 
-  $('addMoreSongs').click(()=>{
+  $('.addMoreSongs').click(()=>{
     let moreSongInputs = `
       <form action="" method="post" class="addSongs">
-        <label>Song URL</label>
+        <label>Song URL:</label>
         <input type="text" name="url" id="">
-        <label> Name</label>
+        <label> Name:</label>
         <input type="text" name="songName" id="">
       </form>`;
-    $('addSongsFormContainer').append(moreSongInputs);
+    $('.addSongsFormContainer').append(moreSongInputs);
   })
 
 
